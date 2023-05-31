1de262d89ea6b7f7019b80c2ee48785fa1a7c41c
Imran Hossain <blur-imran@hotmail.com>
2023-05-31 11:48:42 +0600

    Update student.rb

--- student.rb
+++ student.rb
@@ -2,19 +2,13 @@ require_relative 'person'
 
 class Student < Person
   attr_reader :classroom
-  attr_accessor :name
-
-  def initialize(age, classroom, name: 'Unknown', parent_permission: true)
-    super(age, name: name, parent_permission: parent_permission)
-    @classroom = classroom
-  end
-
-  def play_hooky
-    '¯(ツ)/¯'
-  end
 
   def classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
+  end
+
+  def play_hooky
+    '¯(ツ)/¯'
   end
 end
