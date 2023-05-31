5086f758eccf6a3790688a1f0663713b6b1d4d87
Besufekad Alemu <besurye@gmail.com>
2023-05-31 20:44:18 +0300

    Fix rubocop errors in book_spec.rb

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -15,15 +15,15 @@ describe Book do
     expect(@book).to have_attributes(title: 'Harry Potter', author: 'Imran')
   end
 
-#  it 'test for add_rental' do
-#    person = double('Person', rentals: [])
-#    allow(person).to receive(:age) { '12' }
-#    allow(person).to receive(:name) { 'Imran' }
-#    allow(person).to receive(:parent_permission) { true }
-#    rental = Rental.new(@book, person)
-#    @book.add_rental(rental)
-#    expect(person.name).to eq('Imran')
-#  end
+  #  it 'test for add_rental' do
+  #    person = double('Person', rentals: [])
+  #    allow(person).to receive(:age) { '12' }
+  #    allow(person).to receive(:name) { 'Imran' }
+  #    allow(person).to receive(:parent_permission) { true }
+  #    rental = Rental.new(@book, person)
+  #    @book.add_rental(rental)
+  #    expect(person.name).to eq('Imran')
+  #  end
 
   it 'test for add_rental' do
     person = double('Person', rentals: [])
@@ -34,5 +34,4 @@ describe Book do
     @book.add_rental(rental)
     expect(@book.rentals).to include rental
   end
-
 end
