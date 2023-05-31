0edff05d03b5119904ceb76e52ef8ee22f2817ef
Besufekad Alemu <besurye@gmail.com>
2023-05-31 15:30:28 +0300

    Update options and used SOLID principles in classroom.rb

--- classroom.rb
+++ classroom.rb
@@ -1,16 +1,21 @@
 require_relative 'student'
 
 class Classroom
-  attr_accessor :label
-  attr_reader :students
+  attr_accessor :classroom_label
 
-  def initialize(label)
-    @label = label
+  def initialize(classroom_label)
+    @classroom_label = classroom_label
     @students = []
   end
 
   def add_student(student)
+    return if student.nil?
+
     @students.push(student)
-    student.classroom = self
+    student.classroom= self
+  end
+
+  def students
+    @students.dup.freeze
   end
 end
