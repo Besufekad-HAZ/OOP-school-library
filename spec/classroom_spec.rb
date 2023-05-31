fc32e1f6eda620a96ad669d59e94d36af4f24a52
Imran Hossain <blur-imran@hotmail.com>
2023-05-31 13:17:31 +0600

    Create classroom_spec.rb

---
+++ spec/classroom_spec.rb
@@ -0,0 +1,24 @@
+require_relative '../classroom'
+
+describe Classroom do
+  before :each do
+    @classroom = Classroom.new('Eight')
+  end
+
+  it 'checking classroom instance' do
+    expect(@classroom).to be_instance_of Classroom
+  end
+
+  it 'checking attributes' do
+    expect(@classroom.label).to eq('Eight')
+  end
+
+  it 'test for add student' do
+    student = double('student')
+    allow(student).to receive(:age) { '12' }
+    allow(student).to receive(:name) { 'shubham' }
+    allow(student).to receive(:parent_permission) { true }
+    allow(student).to receive(:classroom=).and_return(Classroom)
+    expect(@classroom.add_student(student).students[0].name).to eq('shubham')
+  end
+end
