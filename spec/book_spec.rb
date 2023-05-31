52b508de546250198f10f1f1fc53c9037ce6da37
Besufekad Alemu <besurye@gmail.com>
2023-05-31 20:33:27 +0300

    Fix an uninitialized constant 'Date' in the 'test for add_rental' error

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,4 +1,6 @@
 require_relative '../book'
+require 'date'
+
 
 describe Book do
   before :each do
@@ -13,13 +15,24 @@ describe Book do
     expect(@book).to have_attributes(title: 'Harry Potter', author: 'Imran')
   end
 
-it 'test for add_rental' do
-  person = double('Person', rentals: [])
-  allow(person).to receive(:age) { '12' }
-  allow(person).to receive(:name) { 'Imran' }
-  allow(person).to receive(:parent_permission) { true }
-  rental = Rental.new(@book, person)
-  @book.add_rental(rental)
-  expect(person.name).to eq('Imran')
-end
+#  it 'test for add_rental' do
+#    person = double('Person', rentals: [])
+#    allow(person).to receive(:age) { '12' }
+#    allow(person).to receive(:name) { 'Imran' }
+#    allow(person).to receive(:parent_permission) { true }
+#    rental = Rental.new(@book, person)
+#    @book.add_rental(rental)
+#    expect(person.name).to eq('Imran')
+#  end
+
+  it 'test for add_rental' do
+    person = double('Person', rentals: [])
+    allow(person).to receive(:age) { '12' }
+    allow(person).to receive(:name) { 'Imran' }
+    allow(person).to receive(:parent_permission) { true }
+    rental = double('Rental', person: person, book: @book, date: Date.today)
+    @book.add_rental(rental)
+    expect(@book.rentals).to include rental
+  end
+
 end
