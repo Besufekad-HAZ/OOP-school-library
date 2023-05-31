fbdf07bbf118d44921f4748f39d71f3c12b1a267
Imran Hossain <blur-imran@hotmail.com>
2023-05-31 12:43:28 +0600

    Update main.rb

--- main.rb
+++ main.rb
@@ -6,28 +6,12 @@ def run_app(handle_options, _app)
   menu_options = {
     '1' => :list_all_books,
     '2' => :list_all_peoples,
-def run_app(handle_options, _app)
-  menu_options = {
-    '1' => :list_all_books,
-    '2' => :list_all_peoples,
     '3' => :create_person,
     '4' => :create_book,
     '5' => :create_rental,
     '6' => :list_rentals,
     '7' => :exit
-    '6' => :list_rentals,
-    '7' => :exit
   }
-  loop do
-    handle_options.display
-    input = gets.chomp
-    if menu_options.key?(input)
-      run = menu_options[input]
-      handle_options.send(run)
-      break if run == 'exit'
-    else
-      puts 'Enter the correct option: '
-    end
   loop do
     handle_options.display
     input = gets.chomp
