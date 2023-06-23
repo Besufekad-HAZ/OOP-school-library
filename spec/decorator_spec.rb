693a0f69243ab31b9e360e5493dac285000f8f3c
Imran Hossain <blur-imran@hotmail.com>
2023-06-23 13:57:33 +0600

    Update decorator_spec.rb

--- spec/decorator_spec.rb
+++ spec/decorator_spec.rb
@@ -4,13 +4,13 @@ describe Decorator do
   before :each do
     person = double('Person', rentals: [])
     allow(person).to receive(:age) { 12 }
-    allow(person).to receive(:name) { 'shubham' }
+    allow(person).to receive(:name) { 'Imran' }
     allow(person).to receive(:parent_permission) { true }
     allow(person).to receive(:correct_name).and_return(person.name)
     @decorator = Decorator.new(person)
   end
 
   it 'checking for correct name method should renturn same name' do
-    expect(@decorator.correct_name).to eq('shubham')
+    expect(@decorator.correct_name).to eq('Imran')
   end
 end
