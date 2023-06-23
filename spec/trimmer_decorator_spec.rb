049042246dc79c0690bd9e12bd739ec3bca73fcc
Imran Hossain <blur-imran@hotmail.com>
2023-06-23 13:57:43 +0600

    Update trimmer_decorator_spec.rb

--- spec/trimmer_decorator_spec.rb
+++ spec/trimmer_decorator_spec.rb
@@ -4,13 +4,13 @@ describe TrimmerDecorator do
   before :each do
     person = double('Person', rentals: [])
     allow(person).to receive(:age) { 12 }
-    allow(person).to receive(:name) { 'shubhamabcdefght' }
+    allow(person).to receive(:name) { 'Hossain' }
     allow(person).to receive(:parent_permission) { true }
     allow(person).to receive(:correct_name).and_return(person.name)
     @trimdecorator = TrimmerDecorator.new(person)
   end
 
   it 'checking for correct name method should return Trimme name' do
-    expect(@trimdecorator.correct_name).to eq('shubhamabc')
+    expect(@trimdecorator.correct_name).to eq('Hossain')
   end
 end
